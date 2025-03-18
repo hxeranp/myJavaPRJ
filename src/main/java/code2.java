b2ef187eae0f7053c5da674c5b16ca3ddf444a92
hxeranp <egg061031@icloud.com>
2025-03-18 18:54:09 +0900

    연습문제

--- src/main/java/code2.java
+++ src/main/java/code2.java
@@ -1,7 +1,7 @@
 public class code2 {
     public static void main(String[] args) {
         int a = 100;
-        int b = 200;
+        int b = 400;
 
         int c = a + b;
         System.out.println(c);
