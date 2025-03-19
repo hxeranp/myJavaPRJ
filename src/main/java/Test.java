a816025b775e321bc0478b0bf8fae978d00bf88d
hxeranp <egg061031@icloud.com>
2025-03-19 17:57:23 +0900

    연습문제

---
+++ src/main/java/Test.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+
+        scanner.close();
+    }
+}
+
+
